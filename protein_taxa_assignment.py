9e6af557eef3bf6e68179e2d671a774a0db692aa
a-kossakowski <106906720+a-kossakowski@users.noreply.github.com>
2023-08-09 22:32:36 +0200

    Update protein_taxa_assignment.py
    Improved commenting for better readability

--- protein_taxa_assignment.py
+++ protein_taxa_assignment.py
@@ -1,40 +1,40 @@
 import os
 import sys
 
-#assigns order of taxonomy for each protein by searchin for it inside
-#proteome file and assigning proteome ID, which is transfered to taxa name
+# This script assigns the order of taxonomy for each protein by searching for it inside
+# a proteome file and assigning a proteome ID, which is transferred to a taxa name.
+# It takes a list of protein IDs and searches for corresponding proteome files in a specified directory.
 
-#provide path and name of the file
-
-#proteomes path
+# Path to the directory containing proteome files.
 proteome_dir = "/home/proteome/"
-#input file as a list of IDs separated by '\n'
+
+# Input file containing a list of protein IDs separated by '\n'.
 id_list_file = sys.argv[1]
 
-#search for proteome files in .faa format (.fasta)
+# Search for proteome files in .faa format (.fasta) within the specified directory.
 proteome_files = [file for file in os.listdir(proteome_dir) if file.endswith(".fasta")]
 
 id_list = []
 
-#read ids from list in file
+# Read IDs from the list in the file.
 with open(id_list_file, 'r') as id_list_file:
     id_list = [line.strip() for line in id_list_file]
 
 id_proteome_map = {}
 
-#initialize ID-proteome mapping
+# Initialize ID-proteome mapping.
 for proteome_file in proteome_files:
-    #join paths to proteomes and proteome files with axtensions specified in 'proteome_files' variable
+    # Join paths to proteomes and proteome files with extensions specified in 'proteome_files' variable.
     proteome_path = os.path.join(proteome_dir, proteome_file)
     with open(proteome_path, 'r') as proteome:
         for line in proteome:
             if line.startswith('>'):
-                id = line[1:].split()[0] 
+                id = line[1:].split()[0]
                 if id in id_list:
                     id_proteome_map[id] = proteome_file
 
-#print ID and proteome for each sequence
+# Print ID and proteome for each sequence.
 for id in id_list:
-    #if no corresponting proteome, prints 'Not found'
+    # If no corresponding proteome is found, prints 'Not found'.
     proteome_file = id_proteome_map.get(id, "Not found")
     print(f"{id}\t{proteome_file}")
